aa8373e21c8903f22d72816b3220571f4c6f1090
dragon818 <dragon.yao1219@gmail.com>
2020-01-31 10:22:07 -0600

    Updates

---
+++ precache-manifest.09b404d9b6862b513eef8c6b049c6482.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1c18d26316f8b0b5900923cbcab30a7",
+    "url": "/socialmedia/index.html"
+  },
+  {
+    "revision": "998634eec7cf9fb01e73",
+    "url": "/socialmedia/static/css/main.3d7b71aa.chunk.css"
+  },
+  {
+    "revision": "6e42c7e996aacf9e19c0",
+    "url": "/socialmedia/static/js/2.74004c5f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/socialmedia/static/js/2.74004c5f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "998634eec7cf9fb01e73",
+    "url": "/socialmedia/static/js/main.3733ea55.chunk.js"
+  },
+  {
+    "revision": "7218587603c7430ee63d",
+    "url": "/socialmedia/static/js/runtime-main.8f5433cd.js"
+  }
+]);
